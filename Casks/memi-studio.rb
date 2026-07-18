9789ba8f69439ea11f00e8d991dfa561cba9721a
sarveshsea <sarveshjax@gmail.com>
2026-07-18 02:21:16 -0500

    chore: update memi-studio cask to 2.5.0

--- Casks/memi-studio.rb
+++ Casks/memi-studio.rb
@@ -1,13 +1,13 @@
 cask "memi-studio" do
-  version "2.4.0"
+  version "2.5.0"
 
   on_arm do
-    sha256 "b99cd6935d86fa23b3049574c5a832a8c826931e5cb7647bad64de6c449af541"
+    sha256 "7755216680a317d2b95e2926b5ff3181eb1f9ca798d428cfe5a47d3402a96f84"
 
     url "https://github.com/sarveshsea/memi-studio/releases/download/v#{version}/Memoire.Studio_#{version}_aarch64.dmg"
   end
   on_intel do
-    sha256 "77957f4c92886ec6a93e965265b6ae52d76ee8e38ed01d8bf14c5e92de1e413a"
+    sha256 "58f5ec7b475199bda8343f840e703f95c32c8eecf01e315df6678c980407c5fd"
 
     url "https://github.com/sarveshsea/memi-studio/releases/download/v#{version}/Memoire.Studio_#{version}_x64.dmg"
   end
